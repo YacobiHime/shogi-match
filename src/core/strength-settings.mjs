430015f97115c999f699bd500bc2b9a95470c6ff
YacobiHime <hare86ame@gmail.com>
2026-08-08 15:45:34 +0900

    最高難易度を人間最高峰の推定値へ調整

--- src/core/strength-settings.mjs
+++ src/core/strength-settings.mjs
@@ -4,7 +4,9 @@ const STRENGTH_SEARCH_SETTINGS = new Map([
   [30000, { nodes: 45000, multiPv: 3, moveRank: { min: 1, max: 3 } }],
   [100000, { nodes: 100000, multiPv: 3, moveRank: { min: 1, max: 3 } }],
   [300000, { nodes: 300000, multiPv: 1, moveRank: { min: 1, max: 1 } }],
-  [1000000, { nodes: 1000000, multiPv: 1, moveRank: { min: 1, max: 1 } }],
+  // CSA会誌Vol.29の人間対局向け実測（48万nodesで将棋倶楽部24のR3381相当）を
+  // 人間最高峰の目安として採用。エンジン差があるため表示上も「推定」とする。
+  [480000, { nodes: 480000, multiPv: 1, moveRank: { min: 1, max: 1 } }],
 ]);
 
 /**
